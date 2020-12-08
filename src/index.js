987b11180abeec613cbc9eab03e5d03d75d818f9
Erhannis Kirran <eyeillus@gmail.com>
2020-12-08 08:29:40 -0500

    Updated comments

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 console.log("AFrameHandMenusTest loading");
+/* // Do like this to replace model
+window.CONTROLLER_MODELS = {};
+window.CONTROLLER_MODELS["vive-controls"] = {type: "json-model", data: {src: 'assets/models/controller_vive.json'}};
+
+// I'm having trouble with model files, so if you want oculus controllers, include the following code here and copy in the model files
+window.CONTROLLER_MODELS["oculus-touch-controls"] = {
+  type: "obj-model",
+  hands: {
+      left: {obj: "assets/models/oculus-left-controller.obj", mtl: "https://cdn.aframe.io/controllers/oculus/oculus-touch-controller-left.mtl"},
+      right: {obj: "assets/models/oculus-right-controller.obj", mtl: "https://cdn.aframe.io/controllers/oculus/oculus-touch-controller-right.mtl"}
+  }
+};
+*/
 require("aframehandmenus");
