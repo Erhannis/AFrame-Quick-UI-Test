3251deb88cfcade2b14ab986e7cb73a2d71febe5
Erhannis Kirran <eyeillus@gmail.com>
2020-12-09 21:57:26 -0500

    Comment

--- src/ui.js
+++ src/ui.js
@@ -2,6 +2,7 @@
  * Example UI setup script
  * Note that most (perhaps all, even) params can be omitted, and there are probably other params not shown here.  See AFrame-Quick-UI/components/quick-ui.js for details.
  * The layouts with page-selection buttons (PageLayout, TabsLayout) take a `side` param for where the buttons should go.
+ * Note that having many elements (e.g., over 100) can impact performance - less when they're not visible, but still some.
  */
 let rInt = function(max) {
   return Math.floor(Math.random() * Math.floor(max));
